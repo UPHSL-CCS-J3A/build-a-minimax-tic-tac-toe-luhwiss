36ab0fd5ef7da3d967178c6248270cea55f6c617
Raxell Constantino <raxellcons26@gmail.com>
2025-10-18 13:57:06 +0800

    Step 6

--- tic-tac-toe_Constantino.py
+++ tic-tac-toe_Constantino.py
@@ -127,3 +127,35 @@ def play_game():
 
 if __name__ == "__main__":
     play_game()
+
+def alphabeta(board, player, alpha=-2, beta=2, me='O', opp='X'):
+    if terminal(board):
+        return utility(board, me, opp), None
+
+ 
+
+    if player == me:
+        best = (-2, None)  # MAX
+        for m in moves(board):
+            b2 = board[:]; b2[m] = player
+            val, _ = alphabeta(b2, opp, alpha, beta, me, opp)
+            if val > best[0]:
+                best = (val, m)
+            alpha = max(alpha, val)
+            if alpha >= beta:  # prune
+                break
+        return best
+    else:
+        best = (2, None)   # MIN
+        for m in moves(board):
+            b2 = board[:]; b2[m] = player
+            val, _ = alphabeta(b2, me, alpha, beta, me, opp)
+            if val < best[0]:
+                best = (val, m)
+            beta = min(beta, val)
+            if alpha >= beta:  # prune
+                break
+        return best
+    
+# Replace inside play_game (AI turn):
+_, m = alphabeta(board, player=ai, alpha=-2, beta=2, me=ai, opp=human)
